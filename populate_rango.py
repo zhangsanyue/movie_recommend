3d90af10320b4714294a26870380445af98761b2
Tongxin Li <2534975L@student.gla.ac.uk>
2021-08-04 15:59:16 +0100

    json

--- populate_rango.py
+++ populate_rango.py
@@ -1,56 +1,66 @@
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movie_recommend.settings')
 
+import json
 import django
 django.setup()
 from movie.models import *
 
-def populate():
-    categories = [
-        {
-            'name': 'comedy',
-            'views': 20,
-            'likes': 32,
-            'movies': [
-                'Space Jam: A New Legacy',
-                'Never Have I Ever',
-                'Rick and Morty',
-            ]
-        },
-        {
-            'name': 'sci-fi',
-            'views': 15,
-            'likes': 56,
-            'movies': [
-                'Black Widow',
-                'Loki',
-                'Dune',
-            ]
-        },
-    ]
 
+def populate():
+
+    with open('data.json', 'r', encoding='utf-8') as f:
+        categories = json.load(f)
 
     for category in categories:
         cat = add_cat(category['name'], category['views'], category['likes'])
-        for movie_title in category['movies']:
-            movie_add(cat, movie_title)
+        for movie in category['movies']:
+            movie_add(cat,
+                movie["average_star"],
+                movie["director"],
+                movie["likes"],
+                movie["stars"],
+                movie["storyline"],
+                movie["title"],
+                movie["views"],
+                movie["writers"]
+            )
 
-
-
-def movie_add(cat, title, views=0):
-    movie = Movie.objects.get_or_create(category=cat, title=title)[0]
-    movie.views=views
-    movie.director="Test Director"
-    movie.writers="Test Writers"
-    movie.stars="Test stars"
-    movie.storyline="Test storyline"
-    movie.average_star=5
-    movie.save()
+def movie_add(cat, average_star, director, likes, stars, storyline, title, views, writers):
+    movie = None
+    try:
+        movie = Movie.objects.get_or_create(category=cat, title=title)[0]
+        movie.views = views
+        movie.director = director
+        movie.writers = writers
+        movie.stars = stars
+        movie.storyline = storyline
+        movie.average_star = average_star
+        # movie.likes = likes
+        movie.save()
+    except:
+        pass
     return movie
 
+
+# def movie_add(cat, title, views=0):
+#     movie = Movie.objects.get_or_create(category=cat, title=title)[0]
+#     movie.views=views
+#     movie.director="Test Director"
+#     movie.writers="Test Writers"
+#     movie.stars="Test stars"
+#     movie.storyline="Test storyline"
+#     movie.average_star=5
+#     movie.save()
+#     return movie
+
 def add_cat(name, views=0, likes=0):
-    c = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
-    c.save()
+    c = None
+    try:
+        c = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
+        c.save()
+    except:
+        pass
     return c
 
 if __name__ == '__main__':
